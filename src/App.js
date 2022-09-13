8c90fc3ecfc3b825a8adb752e34cbc02d1572f06
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-09-14 02:51:53 +0500

    do some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+    <div className='bscontainer'>
+      <div className='row'>
+        <div className='col-lg-4'>
+          <div style={{ boxShadow: "0px 2px 3px 0px rgba(0,0,0,0.75)" }} className='border rounded-md  '>
+            <div>
+              <img className='w-full rounded-md ' src="http://placekitten.com/300/200" alt="service" />
+            </div>
+            <div className='bg-[#f6f6f6] px-3 py-4 '>
+              <h1 className='text-2xl font-semibold uppercase mb-3'>Business Development</h1>
+              <h6 className='text-gray-400'>Business development is the process that is used to identify, nurture and acquire new clients and business opportunities to drive growth and profitability. A business development strategy is a document that describes the strategy you will use to accomplish that goal.</h6>
+              <div className='flex justify-between items-center'>
+                <div className='text-gray-400 text-xs'><span>Full-time</span></div>
+                <div>asdf</div>
+                <div><button className='bg-red-600 hover:bg-green-600 text-white px-5 py-3 rounded-lg text-base'>Apply</button></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col-lg-4'>
+          <div style={{ boxShadow: "0px 2px 3px 0px rgba(0,0,0,0.75)" }} className='border rounded-md  '>
+            <div>
+              <img className='w-full rounded-md ' src="http://placekitten.com/300/200" alt="service" />
+            </div>
+            <div className='bg-[#f6f6f6] px-3 py-4 '>
+              <h1 className='text-2xl font-semibold uppercase mb-3'>Business Development</h1>
+              <h6 className='text-gray-400'>Business development is the process that is used to identify, nurture and acquire new clients and business opportunities to drive growth and profitability. A business development strategy is a document that describes the strategy you will use to accomplish that goal.</h6>
+              <div className='flex justify-between items-center'>
+                <div className='text-gray-400 text-xs'><span>Full-time</span></div>
+                <div>asdf</div>
+                <div><button className='bg-red-600 hover:bg-green-600 text-white px-5 py-3 rounded-lg text-base'>Apply</button></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col-lg-4'>
+          <div style={{ boxShadow: "0px 2px 3px 0px rgba(0,0,0,0.75)" }} className='border rounded-md  '>
+            <div>
+              <img className='w-full rounded-md ' src="http://placekitten.com/300/200" alt="service" />
+            </div>
+            <div className='bg-[#f6f6f6] px-3 py-4 '>
+              <h1 className='text-2xl font-semibold uppercase mb-3'>Business Development</h1>
+              <h6 className='text-gray-400'>Business development is the process that is used to identify, nurture and acquire new clients and business opportunities to drive growth and profitability. A business development strategy is a document that describes the strategy you will use to accomplish that goal.</h6>
+              <div className='flex justify-between items-center'>
+                <div className='text-gray-400 text-xs'><span>Full-time</span></div>
+                <div>asdf</div>
+                <div><button className='bg-red-600 hover:bg-green-600 text-white px-5 py-3 rounded-lg text-base'>Apply</button></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-lg-6'>
+          gggggg
+        </div>
+        <div className='col-lg-6'>
+          gggggg
+        </div>
+      </div>
+    </div>
 
   );
 }
