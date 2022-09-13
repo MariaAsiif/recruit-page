0ee563247884ce0b87ac4bcf6a2b2418120f15a2
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-09-14 01:50:00 +0500

    reset the project delete all previous code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import { lazy, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './assests/style.css';
-// import Loader from './components/loader';
-import Recruit from './pages/recruit';
-// const Home = lazy(() => import("./pages/home"));
+
 
 function App() {
 
-  // React State management hook
-  const [loading, setLoading] = useState(true )
-
-  // React UseEffect update data hook
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //   }, 5000);
-  // }, [])
-
-  // // on load page 
-  // if (loading === true ) {
-  //   return <Loader />
-  // }
-  // else {
-    return (
-      <>
-        <Router>
-          <Routes >
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/" element={<Recruit />} />
-          </Routes >
-        </Router>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>asdfdsaf</h1>
+    </>
+  );
+}
 // }
 
 export default App;
--- src/helper/nameTitle.js
+++
@@ -1,2 +0,0 @@
-export const Title = [
-    "Mr","Mrs","Miss","Ms","Sr","Dr","Dra","Esq","Prof","Don","Miss","Dona","Chica","Chico","Sr.Don","Adv","Sol","B.L","RPh","Sci","Bot","Ad","Mkt"]
