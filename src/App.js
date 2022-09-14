619bcab550e87a7f211a9334d71bc2115846f130
laherasif <laherasif@gmail.com>
2022-09-14 10:42:43 -0700

    add some components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import Carrier from './components/Carrier';
 import Opertunity from './components/Opertunity';
+import Recuiter from './components/Recuit';
 import Standard from './components/Standard';
 
 function App() {
@@ -7,70 +9,8 @@ function App() {
     <div className='mb-4'>
       <Standard />
       <Opertunity />
-      {/* <div className='row'>
-        <div className='col-lg-4'>
-          <div style={{ boxShadow: "0px 2px 3px 0px rgba(0,0,0,0.75)" }} className='border rounded-md  '>
-            <div>
-              <img className='w-full rounded-md ' src="http://placekitten.com/300/200" alt="service" />
-            </div>
-            <div className='bg-[#f6f6f6] px-3 py-4 '>
-              <h1 className='text-2xl font-semibold uppercase mb-3'>Business Development</h1>
-              <h6 className='text-gray-400'>Business development is the process that is used to identify, nurture and acquire new clients and business opportunities to drive growth and profitability. A business development strategy is a document that describes the strategy you will use to accomplish that goal.</h6>
-              <div className='flex justify-between items-center'>
-                <div className='text-gray-400 text-xs'><span>Full-time</span></div>
-                <div>asdf</div>
-                <div><button className='bg-red-600 hover:bg-green-600 text-white px-5 py-3 rounded-lg text-base'>Apply</button></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='col-lg-4'>
-          <div style={{ boxShadow: "0px 2px 3px 0px rgba(0,0,0,0.75)" }} className='border rounded-md  '>
-            <div>
-              <img className='w-full rounded-md ' src="http://placekitten.com/300/200" alt="service" />
-            </div>
-            <div className='bg-[#f6f6f6] px-3 py-4 '>
-              <h1 className='text-2xl font-semibold uppercase mb-3'>Business Development</h1>
-              <h6 className='text-gray-400'>Business development is the process that is used to identify, nurture and acquire new clients and business opportunities to drive growth and profitability. A business development strategy is a document that describes the strategy you will use to accomplish that goal.</h6>
-              <div className='flex justify-between items-center'>
-                <div className='text-gray-400 text-xs'><span>Full-time</span></div>
-                <div>asdf</div>
-                <div><button className='bg-red-600 hover:bg-green-600 text-white px-5 py-3 rounded-lg text-base'>Apply</button></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='col-lg-4'>
-          <div style={{ boxShadow: "0px 2px 3px 0px rgba(0,0,0,0.75)" }} className='border rounded-md  '>
-            <div>
-              <img className='w-full rounded-md ' src="http://placekitten.com/300/200" alt="service" />
-            </div>
-            <div className='bg-[#f6f6f6] px-3 py-4 '>
-              <h1 className='text-2xl font-semibold uppercase mb-3'>Business Development</h1>
-              <h6 className='text-gray-400'>Business development is the process that is used to identify, nurture and acquire new clients and business opportunities to drive growth and profitability. A business development strategy is a document that describes the strategy you will use to accomplish that goal.</h6>
-              <div className='flex justify-between items-center'>
-                <div className='text-gray-400 text-xs'><span>Full-time</span></div>
-                <div>asdf</div>
-                <div><button className='bg-red-600 hover:bg-green-600 text-white px-5 py-3 rounded-lg text-base'>Apply</button></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='row p-16'>
-        <div className='col-lg-6'>
-          <div>
-            <h1>I AM RECRUITER!</h1>
-            <p>A box-shadow CSS generator that helps you quickly generate box-shadow CSS declarations for your website. It comes with many options and it demonstrates ...</p>
-          </div>
-        </div>
-        <div className='col-lg-6'>
-          <div>
-            <h1>I AM JOBSEEKER!</h1>
-            <p>A box-shadow CSS generator that helps you quickly generate box-shadow CSS declarations for your website. It comes with many options and it demonstrates ...</p>
-          </div>
-        </div>
-      </div> */}
+      <Carrier />
+      <Recuiter/>
     </div>
 
   );
