400bfb21f2d1c5c267dccd6f75e314246a3837a9
laherasif <laherasif@gmail.com>
2022-09-19 04:15:27 -0700

    resume heading

--- src/components/Banner/Banners.jsx
+++ src/components/Banner/Banners.jsx
@@ -20,7 +20,6 @@ export const ResumeBanner = () => {
                 <div className='col-lg-12 relative'>
                     <div className={`bg-resume-background lg:h-full bg-bottom bg-no-repeat bg-cover w-full`}>
                     </div>
-                        <h2 className=' absolute bottom-0 top-[50%] text-[35px] font-semibold left-0 right-0 text-center text-white'>Join us on our journey</h2>
                 </div>
             </div>
         </div>
