72d47a8ab34b3e4678fcc82dc5177c97c2312e62
laherasif <laherasif@gmail.com>
2022-09-22 23:37:49 -0700

    login page created

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import HomePage from './Pages/HomePage';
 import { RecruitPage } from './Pages/RecruitPage';
 import JoinUs from './Pages/JoinUs';
 import Resume from './Pages/Resume';
+import Login from './Pages/Login';
+import Verify from './Pages/Verify';
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/verify" element={<Verify/>}/>
         <Route path="/" element={<HomePage />} />
         <Route path="/recruit" element={<RecruitPage />} />
         <Route path="/joinUs" element={<JoinUs/>}/>
         <Route path="/detail" element={<Resume/>}/>
+        
         
       </Routes>
 
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import loginImage from '../images/login/Login.png'
+import logoImage from '../images/logo.png'
+import { IoMail } from 'react-icons/io5'
+import { FaLock } from 'react-icons/fa'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
+const Login = () => {
+    const [login, setLogin] = useState({ email: '', password: '' })
+    const [show, setShow] = useState(false)
+
+    const handleChange = (e) => {
+        const { value, name } = e.target
+
+        setLogin((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-4'>
+                    <img src={loginImage} className="h-[625px]" alt="login_image" />
+                </div>
+                <div className='col-lg-8 '>
+                    <div className=' max-w-[400px] m-auto mt-[5rem] '>
+                        <img src={logoImage} className="ml-[80px] w-[40%]" alt="logo" />
+                        <div className='mt-[5rem]'>
+                            <div className='border mb-4 border-[#E84025] cursor-pointer hover:border-[#93C234] rounded-md flex items-center'>
+                                <IoMail className='text-[18px] ml-2 mr-2 text-[#E84025]' />
+                                <input type="email" onChange={handleChange} name="email" value={login.email} placeholder='Login Email:' className='focus:outline-none text-[#93C234] w-full rounded-md  h-[45px] ' />
+                            </div>
+                            <div className={`${login.password && 'hover:border-[#93C234]'} border mb-4 border-[#E84025] cursor-pointer rounded-md flex items-center'`}>
+                                <FaLock className={`${login.password ? 'text-[18px] mt-3 ml-2 mr-2 text-[#93C234]' : 'text-[18px] ml-2 mr-2 mt-3 text-[#E84025]'}`} />
+                                <input type={show ? "text" : "password"} onChange={handleChange} name="password" value={login.password} placeholder='Password:' className='focus:outline-none text-[#93C234] w-full rounded-md  h-[45px] ' />
+                                {login.password ?
+                                    show ?
+                                        <AiFillEye onClick={() => setShow(false)} className={'text-[27px] mr-2 mt-2 text-[#93C234]'} />
+                                        :
+                                        <AiFillEyeInvisible onClick={() => setShow(true)} className={'text-[27px] mr-2 mt-2 text-[#93C234]'} />
+                                    : null 
+                                   
+                                }
+                            </div>
+                        </div>
+                        <div className='mt-4 flex items-center'>
+                            <input type="checkbox" className='text-gray-400' />
+                            <span className='text-[12px] ml-2 text-gray-400'>Lorem ipsum dolor sit amet</span>
+                        </div>
+                        <div className='text-center mt-4' >
+                            <button className='border w-[50%] h-[45px] rounded-md text-white hover:bg-[#93C234] bg-[#E84025] '>Login</button>
+                        </div>
+                        <div className='mt-4'>
+                            <span className='text-[12px] text-gray-400'>
+                                Don't have an account to login,
+                                <span className='underline text-[#E84025] text-[12px] pl-1'>Click here to create the account!</span>
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
