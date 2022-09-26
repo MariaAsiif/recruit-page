b53a309a18d6ff791f229e29df1a87f6a18a697b
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-09-26 23:25:57 +0500

    Update AppliedJobs.jsx

--- src/Pages/AppliedJobs.jsx
+++ src/Pages/AppliedJobs.jsx
@@ -2,7 +2,73 @@ import React from 'react'
 
 const AppliedJobs = () => {
     return (
-        <div>AppliedJobs</div>
+        <div className='bscontainer h-screen py-5'>
+            <div className='row'>
+                <div className='col-g-12'>
+                    <div className="overflow-x-auto">
+                        <table className="table-auto w-full bg-white rounded-md">
+                            {/* Table header */}
+                            <thead className="text-xs font-semibold uppercase text-slate-500  border-b border-slate-200">
+                                <tr className='text-[#626973]'>
+
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+                                        <span className="font-semibold text-left">Job ID</span>
+                                    </th>
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="font-semibold text-left">Job Title</div>
+                                    </th>
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="font-semibold text-left">Company Name</div>
+                                    </th>
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="font-semibold text-left">Job Type</div>
+                                    </th>
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="font-semibold text-left">Application Date</div>
+                                    </th>
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="font-semibold text-left">Status</div>
+                                    </th>
+                                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="font-semibold text-left">Action</div>
+                                    </th>
+
+                                </tr>
+                            </thead>
+                            {/* Table body */}
+                            <tbody className="text-sm divide-y divide-slate-200">
+                                <tr className='text-[#999FA9]'>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">#34534</div>
+                                    </td>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">34534</div>
+                                    </td>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">34534</div>
+                                    </td>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">34534</div>
+                                    </td>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">34534</div>
+                                    </td>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">34534</div>
+                                    </td>
+                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                        <div className="text-left">34534</div>
+                                    </td>
+
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
