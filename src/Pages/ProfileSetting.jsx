431924ed12280c85f5a6ae292255277f630b0c07
laherasif <laherasif@gmail.com>
2022-09-26 02:22:37 -0700

    profile and dashboard done

--- src/Pages/ProfileSetting.jsx
+++ src/Pages/ProfileSetting.jsx
@@ -1,8 +1,104 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import profileImg from '../images/people.png'
+import { FiEdit } from 'react-icons/fi'
+import { AiOutlineComment } from 'react-icons/ai'
+import { FaUser } from 'react-icons/fa'
+import { BiLockOpenAlt } from 'react-icons/bi'
+import { IoShieldCheckmark } from 'react-icons/io5'
+import InfoForm from '../components/profileStep/InfoForm'
+import CompanyForm from '../components/profileStep/CompanyForm'
+import ResumeForm from '../components/profileStep/ResumeForm'
+import SubmitForm from '../components/profileStep/SubmitForm'
 const ProfileSetting = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const handleNext =() => {
+        let updated = activeIndex + 1
+        setActiveIndex(updated)
+    }
     return (
-        <div>ProfileSetting</div>
+        <div className='bscontainer-fluid p-[30px] h-full '>
+            <div className=''>
+                <h2 className='text-[20px] font-semibold'>Here, we found job!</h2>
+                <span className='text-[12px] text-gray-400' >Brand on your preferance</span>
+            </div>
+            <div className='row mt-2 ml-2'>
+                <div className='col-lg-4 '>
+                    <div className='bg-white text-center p-5 '>
+                        <div className='relative'>
+                            <div className='flex justify-center items-center'>
+                                <img src={profileImg} className="w-[38%] border border-[#65A33A] pt-[3px] rounded-full" alt="profile" />
+                            </div>
+                            <div className='absolute  bottom-[8%] left-[60%] p-1  rounded-full bg-[#E84025] '>
+                                <FiEdit className='text-[white]' />
+                            </div>
+                        </div>
+                        <div className='mt-2'>
+                            <h2 className='text-[20px] font-medium'>Laher asif</h2>
+                            <span className='text-[13px] text-gray-400'>Web Developer</span>
+                        </div>
+                    </div>
+                    <div className='bg-white text-center p-5 mt-4 '>
+                        <div className='flex justify-center'>
+
+                            <AiOutlineComment className='text-gray-400 text-[5rem]' />
+
+                        </div>
+                        <div className='mt-2'>
+                            <h2 className='text-[20px] font-medium'>Laher asif</h2>
+                            <span className='text-[13px] text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cumque? Explicabo cumque deserunt quidem, omnis modi molestias doloremque eligendi tempore? Consequatur, expedita? Unde natus optio esse nostrum magni aspernatur fuga.</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-lg-8'>
+
+                    <div className=" bg-white rounded-md p-5">
+                        <div className='flex'>
+                            <div>left image</div>
+                            <div>
+                                <div>
+                                    <h2 className='text-[20px]  text-[#626973] font-medium'>Your account need to be verified </h2>
+                                    <span className='text-[11px] text-[#626973]'>If you need to update the information you will be able to verify your account. You need to verify your Phone number and Email account</span>
+                                </div>
+                                <div className='flex  flex-wrap justify-center items-center lg:px-14 px-1 '>
+                                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                                        <div className={`${activeIndex === 0 ? 'rounded-full border bg-[#65A33A] text-white' : 'rounded-full border bg-gray-300 text-white'}  text-white pt-2 text-sm w-[50px] h-[50px] rounded-full border`}>
+                                            <FaUser className='ml-[14px] mt-[6px] text-[20px] ' />
+                                        </div>
+                                    </div>
+                                    <div className={`${activeIndex === 1 ? 'bg-gradient-to-r from-[#65A33A] to-[#65A33A]' : 'bg-gray-100'} md:w-[25%] md:-ml-5 h-1 lg:w-[28%] w-[22.6%]`}></div>
+                                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                                        <div className={`${activeIndex === 1 ? 'rounded-full border bg-[#65A33A] text-white' : 'rounded-full border bg-gray-300 text-white'}  text-white pt-2 text-sm w-[50px] h-[50px] rounded-full border`}>
+                                            <BiLockOpenAlt className='ml-[14px] mt-[6px] text-[20px] ' />
+                                        </div>
+                                    </div>
+                                    <div className={`${activeIndex === 2 ? 'bg-gradient-to-r from-[#65A33A] to-[#65A33A]' : 'bg-gray-100'} md:w-[25%] md:-ml-5 h-1 lg:w-[28%] w-[22.6%]`}></div>
+                                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                                        <div className={`${activeIndex === 2 ? 'rounded-full border bg-[#65A33A] text-white' : 'rounded-full border bg-gray-300 text-white'}  text-white pt-2 text-sm w-[50px] h-[50px] rounded-full border`}>
+                                            <IoShieldCheckmark className='ml-[14px] mt-[6px] text-[20px] ' />
+                                        </div>
+                                    </div>
+                                    <div className={`${activeIndex === 3 ? 'bg-gradient-to-r from-[#65A33A] to-[#65A33A]' : 'bg-gray-100'} md:w-[25%] md:-ml-5  h-1 lg:w-[28%] w-[22.6%]`}></div>
+                                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                                        <div className={`${activeIndex === 3 ? 'rounded-full border bg-[#65A33A] text-white' : 'rounded-full border bg-gray-300 text-white'}  text-white pt-2 text-sm w-[50px] h-[50px] rounded-full border`}>
+                                            <BiLockOpenAlt className='ml-[14px] mt-[6px] text-[20px] ' />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='bg-white rounded-md p-5 mt-5'>
+                        {activeIndex === 0 && <InfoForm handleNext={handleNext}  />}
+                        {activeIndex === 1 && <ResumeForm handleNext={handleNext}  />}
+                        {activeIndex === 2 && <CompanyForm handleNext={handleNext}  />}
+                        {activeIndex === 3 && <SubmitForm handleNext={handleNext}  />}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/SharedComponents/DashboardHeader.jsx
+++ src/components/SharedComponents/DashboardHeader.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BiSearch } from "react-icons/bi";
-
+import { AiTwotoneCloud } from 'react-icons/ai'
+import { IoMdNotificationsOutline } from 'react-icons/io'
+import profileImage from '../../images/people.png'
 const DashboardHeader = ({ sidebarOpen, setSidebarOpen }) => {
     return (
         <header style={{ boxShadow: "0px 0px 6px #00000029" }} className="sticky top-0 ml-1 bg-white border-b border-slate-200 z-30">
@@ -18,14 +20,40 @@ const DashboardHeader = ({ sidebarOpen, setSidebarOpen }) => {
                     </div>
 
                     {/* Header: Right side */}
-                    <div className="   lg:w-[100%] w-[90%]">
-                        <div className='row'>
-                            <div className='col-lg-4 lg:flex hidden   '>
-                                <input className='bg-[#F3F6FB] focus:outline-none w-[96%] py-1 px-2 text-xs text-[#999FA9]' placeholder='search' />
+                    <div className="lg:w-[100%] ">
+                        <div className='row '>
+                            <div className='col-lg-6 flex    '>
+                                <input className='bg-[#F3F6FB] h-auto focus:outline-none lg:w-[100%] py-1 px-2 text-xs text-[#999FA9]' placeholder='search' />
                                 <button className='bg-[#E84025] rounded-sm w-[8%] ml-2'><BiSearch className='inline text-white' /></button>
                             </div>
-                            <div className='col-lg-6'>
-                                asdfdadsf
+                            <div className='col-lg-6 '>
+                                <div className='flex lg:items-center'>
+                                    <div className='lg:flex lg:items-center lg:border-l  lg:p-2 border-r hidden'>
+                                        <AiTwotoneCloud className='text-[20px]' />
+                                        <span className='ml-2 text-sm'>Rain-1'C</span>
+                                    </div>
+                                    <div className='lg:flex lg:items-center lg:border-l  lg:p-2 border-r hidden'>
+                                        <select className='focus:outline-none'>
+                                            <option>Islamabad</option>
+                                        </select>
+                                    </div>
+                                    <div className='lg:flex lg:items-center lg:border-l  lg:p-2 border-r hidden'>
+                                        <img src={profileImage} className=" ml-3 w-6" alt="profile" />
+                                        <select className='focus:outline-none'>
+                                            <option>Islamabad</option>
+                                        </select>
+                                    </div>
+                                    <div className='flex items-center  p-2'>
+                                        <IoMdNotificationsOutline className='text-[20px]' />
+                                        <img src={profileImage} className=" ml-3 w-6" alt="profile" />
+                                        <select className='focus:outline-none'>
+                                            <option>Islamabad</option>
+                                        </select>
+                                    </div>
+
+
+
+                                </div>
                             </div>
                         </div>
                     </div>
