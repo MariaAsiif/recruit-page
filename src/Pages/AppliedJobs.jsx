b0d380b97619a367be6df514e02e15afeccdb6ea
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-09-27 00:27:00 +0500

    fix action column add dropdown

--- src/Pages/AppliedJobs.jsx
+++ src/Pages/AppliedJobs.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const AppliedJobs = () => {
+    const status = [
+        { name: "Pending", color: "yellow" },
+        { name: "Accepted", color: "green" },
+        { name: "Rejected", color: "red" },
+        { name: "On the way", color: "gray" },]
     return (
         <div className='bscontainer h-screen py-5'>
             <div className='row'>
@@ -37,30 +42,75 @@ const AppliedJobs = () => {
                             </thead>
                             {/* Table body */}
                             <tbody className="text-sm divide-y divide-slate-200">
-                                <tr className='text-[#999FA9]'>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">#34534</div>
-                                    </td>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">34534</div>
-                                    </td>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">34534</div>
-                                    </td>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">34534</div>
-                                    </td>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">34534</div>
-                                    </td>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">34534</div>
-                                    </td>
-                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                        <div className="text-left">34534</div>
-                                    </td>
+                                {status.map((status) => {
+                                    return (
+                                        <tr className='text-[#999FA9]'>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="text-left">#34534</div>
+                                            </td>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className='flex place-items-center'>
+                                                    <img className=' w-full h-auto max-w-[30px] rounded-md' src={"http://placekitten.com/100/100"} alt="logo" loading="lazy" />
+                                                    <h1 className='font-semibold text-[#626973] ml-2'>UI/UX DESIGNER</h1>
+                                                </div>
+                                            </td>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="text-left">
+                                                    <img className=' w-full h-auto max-w-[20px] rounded-full inline-block mr-2' src={"http://placekitten.com/100/100"} alt="logo" loading="lazy" />
+                                                    Google
+                                                </div>
+                                            </td>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="text-left">Full Time</div>
+                                            </td>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="text-left">02 September 22</div>
+                                            </td>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className={`border border-${status.color}-500  bg-${status.color}-50   text-${status.color}-500 px-2 py-2 text-center font-semibold rounded-md`}>{status.name}</div>
+                                            </td>
+                                            <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="text-center ">
+                                                    <div className="dropdown relative">
+                                                        <button className=" dropdown-toggle px-2 text-[#626973] font-medium text-lg leading-tight uppercase rounded    focus:outline-none focus:ring-0   transition duration-150 ease-in-out   " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"> ... </button>
+                                                        <ul className=" dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-center font-medium rounded-lg shadow-2xl mt-1 hidden m-0 bg-clip-padding border-none" aria-labelledby="dropdownMenuButton1">
+                                                            <li>
+                                                                <span className=" dropdown-item text-sm py-2 px-4 cursor-pointer block w-full whitespace-nowrap bg-transparent text-[#626973] hover:bg-gray-100 " >Cancel</span>
+                                                            </li>
+                                                            <li>
+                                                                <span className=" dropdown-item text-sm py-2 px-4 cursor-pointer block w-full whitespace-nowrap bg-transparent text-[#E84025] hover:bg-gray-100 " >Cancel Request</span>
+                                                            </li>
+                                                            <li>
+                                                                <span className=" dropdown-item text-sm py-2 px-4 cursor-pointer block w-full whitespace-nowrap bg-transparent text-[#626973] hover:bg-gray-100 " >Details</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
 
-                                </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                </div>
+                                            </td>
+
+                                        </tr>
+                                    )
+                                })}
+
 
                             </tbody>
                         </table>
