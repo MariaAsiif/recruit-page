005ac9b96ea5b42525df5b498e081caa1dec4181
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-09-29 00:39:30 +0500

    update stepper

--- src/components/ProfileSettingComponents/ProfilerStepper.jsx
+++ src/components/ProfileSettingComponents/ProfilerStepper.jsx
@@ -12,28 +12,28 @@ const ProfilerStepper = () => {
             </div>
             <div className='w-5/6 '>
                 <div className='flex flex-wrap justify-center items-center '>
-                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                    <div className='lg:w-[6%] w-[8%] text-center relative'>
                         <div className={` rounded-full bg-[#65A33A] text-white pt-2 text-sm  w-[40px] h-[40px] `}>
                             <ImUser size={24} className='inline' />
                         </div>
 
                     </div>
-                    <div className={` bg-[#65A33A]  md:w-[25%] md:-ml-5 h-[1px] lg:w-[28%] w-[22.6%]`}></div>
-                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                    <div className={` bg-[#65A33A]  md:w-[25%] md:-ml-5 h-[1px] lg:w-[25%] w-[22.6%]`}></div>
+                    <div className='lg:w-[6%] w-[8%] text-center relative'>
                         <div className={`bg-[#dfe0e0] text-[#999FA9] pt-2 text-sm w-[40px] h-[40px] rounded-full`}>
                             <MdOutlineLockOpen size={24} className='inline' />
                         </div>
 
                     </div>
-                    <div className={`bg-[#dfe0e0] md:w-[25%] md:-ml-5 h-[1px] lg:w-[28%] w-[22.6%]`}></div>
-                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                    <div className={`bg-[#dfe0e0] md:w-[25%] md:-ml-5 h-[1px] lg:w-[25%] w-[22.6%]`}></div>
+                    <div className='lg:w-[6%] w-[8%] text-center relative'>
                         <div className={`bg-[#dfe0e0] text-[#999FA9] pt-2 text-sm w-[40px] h-[40px] rounded-full`}>
                             <BsShieldFillCheck size={24} className='inline' />
                         </div>
 
                     </div>
-                    <div className={`bg-[#dfe0e0] md:w-[25%] md:-ml-5  h-[1px] lg:w-[28%] w-[22.6%]`}></div>
-                    <div className='lg:w-[4%] w-[8%] text-center relative'>
+                    <div className={`bg-[#dfe0e0] md:w-[25%] md:-ml-5  h-[1px] lg:w-[25%] w-[22.6%]`}></div>
+                    <div className='lg:w-[6%] w-[8%] text-center relative'>
                         <div className={`bg-[#dfe0e0] text-[#999FA9] pt-2 text-sm w-[40px] h-[40px] rounded-full`}>
                             <BsCheck2All size={24} className='inline' />
                         </div>
