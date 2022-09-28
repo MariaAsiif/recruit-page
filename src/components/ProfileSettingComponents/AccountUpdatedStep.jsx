fd39224cb9cad2e52ee817d49cad10ec43df3a41
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-09-29 02:08:42 +0500

    add animation success

--- src/components/ProfileSettingComponents/AccountUpdatedStep.jsx
+++ src/components/ProfileSettingComponents/AccountUpdatedStep.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import confirmation_animation from "../../confirmation.json"
+import Lottie from "lottie-react"
 const AccountUpdatedStep = () => {
     return (
         <>
-            <div className='col-lg-12 mb-10 text-center pt-28'>
+            <div className='col-lg-12 mb-10 text-center lg:pt-28 pt-11'>
                 <h1 className='text-[#65A33A] font-sans font-semibold text-lg mb-2'>Account Updated</h1>
                 <h1 className='text-[#999FA9] text-[10px] font-light mb-2'>Click on verify and get OTP code to verify your account</h1>
+                <div className='w-56 m-auto'>
+                    <Lottie animationData={confirmation_animation} />
+                </div>
+
             </div>
         </>
     )
