963facef9501a9bc991ab6d76dc36ebf0d019757
laherasif <laherasif@gmail.com>
2022-09-29 09:46:11 -0700

    add link lexicon

--- src/components/Footer/FooterSection.jsx
+++ src/components/Footer/FooterSection.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const FooterSection = ({heading , data }) => {
+const FooterSection = ({ heading, data }) => {
     return (
         <div className='mt-4'>
             <h2 className='font-semibold text-[16px]'>{heading}</h2>
             {
                 data.map((comp, index) => (
                     <ul key={index} className="mt-1">
-                        <li className='text-[14px] cursor-pointer text-gray-400'>{comp}</li>
+                        <Link to="/lexicon">
+                            <li className='text-[14px] cursor-pointer text-gray-400'>{comp}</li>
+                        </Link>
                     </ul>
                 ))
             }
