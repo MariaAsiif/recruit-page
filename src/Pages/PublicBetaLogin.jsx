8d036825f9a9add51360903f1e067303bd203302
laherasif <laherasif@gmail.com>
2022-10-03 01:39:28 -0700

    add some content

--- src/Pages/PublicBetaLogin.jsx
+++ src/Pages/PublicBetaLogin.jsx
@@ -33,7 +33,16 @@ import { BsLinkedin, BsTwitter } from 'react-icons/bs'
 import { toast, ToastContainer } from 'react-toastify'
 import { RiMessage2Line } from 'react-icons/ri'
 import beta from '../assets/images/beta.png'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
+const schema = yup.object({
+    email: yup.string().email("Invalid email").required("Enter your valid email address"),
+    password: yup.string().required("Enter your chosen password"),
+
+
+});
 const PublicBetaLogin = () => {
     const [login, setLogin] = useState({ email: '', password: '' })
     const [show, setShow] = useState(false)
@@ -54,6 +63,10 @@ const PublicBetaLogin = () => {
         slider5,
 
     ]
+
+
+    const { register, watch, reset, handleSubmit, formState: { errors } } = useForm({ mode: 'onChange', resolver: yupResolver(schema) });
+
 
 
     const handleChange = (e) => {
@@ -143,6 +156,8 @@ const PublicBetaLogin = () => {
         console.log("Data")
     }
 
+
+
     return (
         <div className='container h-screen'>
             <ToastContainer
@@ -179,7 +194,7 @@ const PublicBetaLogin = () => {
 
                         {Images.map((img, index) => (
 
-                            <SwiperSlide  >
+                            <SwiperSlide key={index} >
                                 <video preload="true" autoplay="autoplay" loop="loop">
                                     <source src={img} type="video/mp4" />
                                 </video>
@@ -220,105 +235,119 @@ const PublicBetaLogin = () => {
 
                         </div>
                     </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className='lg:max-w-[450px] w-full m-auto  text-center page_wrapper'>
+                            <img onClick={() => setplay(true)} src={logoImage} className="lg:ml-[85px] w-[40%]" alt="logo" />
+                            {term && <PopUp permition={term} lang={lang} Toggle={setterm} />}
+                            {about && <Whatabout lang={lang} permition={true} Toggle={setAbout} />}
+                            <div className='mt-[1rem]'>
 
-                    <div className='lg:max-w-[450px] w-full m-auto  text-center page_wrapper'>
-                        <img onClick={() => setplay(true)} src={logoImage} className="lg:ml-[85px] w-[40%]" alt="logo" />
-                        {term && <PopUp permition={term} lang={lang} Toggle={setterm} />}
-                        {about && <Whatabout lang={lang} permition={true} Toggle={setAbout} />}
-                        <div className='mt-[3rem]'>
+                                <div className='text-center flex justify-center items-center mb-4'>
+                                    <h2 className='flex items-center  text-[30px] text-[#93C234]'>Public <span className=' text-[#E84025] ml-1'>Beta</span> <span className='text-[17px] text-black pt-3 pl-2 font-bold'> v.1.1</span></h2>
+                                </div>
 
-                            <div className='text-center flex justify-center items-center mb-4'>
-                                <h2 className='flex items-center  text-[30px] text-[#93C234]'>Public <span className=' text-[#E84025] ml-1'>Beta</span> <span className='text-[17px] text-black pt-3 pl-2 font-bold'> v.1.1</span></h2>
+                                <div className='border   cursor-pointer  rounded-md flex items-center'>
+                                    <IoMail className={`${watch('email') ? 'text-[20px]  ml-2 mr-2 text-[#93C234]' : 'text-[20px] ml-2 mr-2  '}`} />
+                                    <input type={show ? "password" : "email"} {...register('email')} name="email" placeholder='John@gmail.com' className='focus:outline-none text-[#93C234] w-full rounded-md  h-[45px] ' />
+                                    {
+                                        show ?
+                                            <AiFillEyeInvisible onClick={() => setShow(false)} className={`${watch('email') && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
+                                            :
+                                            <AiFillEye onClick={() => setShow(true)} className={` ${watch('email') && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
+
+
+                                    }
+                                </div>
+                                <div>
+                                    {errors.email && (
+                                        <p className="text-red-500 text-sm">{errors.email.message}</p>
+                                    )}
+                                </div>
+                                <div className={` border mt-2  cursor-pointer rounded-md flex items-center`}>
+                                    <FaLock className={`${watch('password') ? 'text-[18px]  ml-2 mr-2 text-[#93C234]' : 'text-[18px] ml-2 mr-2 '}`} />
+                                    <input type={showpass ? "text" : "password"} {...register('password')} name="password" placeholder='Password' className='focus:outline-none text-[#93C234] w-full rounded-md  h-[45px] ' />
+                                    {/* <HiCheckCircle className={`${login.password && 'text-[#93C234]'} text-gray-400 text-[1.5rem] mr-1`} /> */}
+                                    {
+                                        showpass ?
+                                            <AiFillEyeInvisible onClick={() => setShowPass(false)} className={`${watch('password') && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
+                                            :
+                                            <AiFillEye onClick={() => setShowPass(true)} className={` ${watch('password') && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
+
+
+                                    }
+                                </div>
+                                {errors.password && (
+                                    <p className="text-red-500 text-sm">{errors.password.message}</p>
+                                )}
+                            </div>
+                            <div className='mt-[1.2rem] flex items-center check_term ' >
+                                <input type="checkbox" className='w-[15px] h-[15px] ml-2' onChange={handleValue} />
+                                <span className='text-[13px] ml-[10px] text-gray-400'>Accept terms and condition</span>
+                            </div>
+                            <div className='mt-[10px] flex items-center check_term ' >
+                                <input type="checkbox" className='w-[15px] h-[15px] ml-2' onChange={handleValue} />
+                                <span className='text-[13px] ml-[10px] text-gray-400'>Remember me</span>
+                            </div>
+                            <div>
+                                <div className="hover:underline text-sm mt-2 cursor-pointer ml-[33px] text-black text-left hover:text-[#93C234]" onClick={() => setAbout(true)}>What is beta ?</div>
                             </div>
 
-                            <div className='border mb-5  cursor-pointer  rounded-md flex items-center'>
-                                <IoMail className={`${login.email ? 'text-[20px]  ml-2 mr-2 text-[#93C234]' : 'text-[20px] ml-2 mr-2  '}`} />
-                                <input type={show ? "password" : "email"} onChange={handleChange} name="email" value={login.email} placeholder='John@gmail.com' className='focus:outline-none text-[#93C234] w-full rounded-md  h-[45px] ' />
-                                {
-                                    show ?
-                                        <AiFillEyeInvisible onClick={() => setShow(false)} className={`${login.email && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
-                                        :
-                                        <AiFillEye onClick={() => setShow(true)} className={` ${login.email && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
 
-
-                                }
-                            </div>
-                            <div className={` border mb-4  cursor-pointer rounded-md flex items-center`}>
-                                <FaLock className={`${login.password ? 'text-[18px]  ml-2 mr-2 text-[#93C234]' : 'text-[18px] ml-2 mr-2 '}`} />
-                                <input type={showpass ? "password" : "text"} onChange={handleChange} name="password" value={login.password} placeholder='Password' className='focus:outline-none text-[#93C234] w-full rounded-md  h-[45px] ' />
-                                {/* <HiCheckCircle className={`${login.password && 'text-[#93C234]'} text-gray-400 text-[1.5rem] mr-1`} /> */}
-                                {
-                                    showpass ?
-                                        <AiFillEyeInvisible onClick={() => setShowPass(false)} className={`${login.password && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
-                                        :
-                                        <AiFillEye onClick={() => setShowPass(true)} className={` ${login.password && 'text-[#93C234]'} text-[27px] mr-2 mt-2 `} />
-
-
-                                }
-                            </div>
-                        </div>
-                        <div className='mt-[1.2rem] flex items-center check_term ' >
-                            <input type="checkbox" className='w-[15px] h-[15px] ml-2' onChange={handleValue} />
-                            <span className='text-[13px] ml-[10px] text-gray-400'>Accept terms and condition</span>
-                        </div>
-                        <div>
-                            <div className="hover:underline text-sm mt-2 cursor-pointer ml-[33px] text-black text-left hover:text-[#93C234]" onClick={() => setAbout(true)}>What is beta ?</div>
-                        </div>
-
-                        {/* <button onClick={changeLanguage}>
+                            {/* <button onClick={changeLanguage}>
                             {lang === "en" ? "English" : "spanish"}
                         </button> */}
 
-                        <div className='lg:flex lg:justify-between justify-center  items-center mt-5 bg-gray-100 p-3'>
-                            <div className='flex items-center '>
-                                <div className=' border rounded-full p-2 bg-green-500 text-white'>
-                                    <AiFillLock className='text-[14px]' />
-                                </div>
-                                <h2 className='flex flex-col text-left ml-2 font-bold text-[15px]'>
-                                    SSL
-                                    <br />
-                                    <span className='font-medium'>encryption</span>
+                            <div className='lg:flex lg:justify-between justify-center  items-center mt-5 bg-gray-100 p-3'>
+                                <div className='flex items-center '>
+                                    <div className=' border rounded-full p-2 bg-green-500 text-white'>
+                                        <AiFillLock className='text-[14px]' />
+                                    </div>
+                                    <h2 className='flex flex-col text-left ml-2 font-bold text-[15px]'>
+                                        SSL
+                                        <br />
+                                        <span className='font-medium'>encryption</span>
 
-                                </h2>
+                                    </h2>
+                                </div>
+                                <div className='flex items-center ml-1 mt-1'>
+                                    <div className='  rounded-full '>
+                                        <IoMdClock className='text-[2rem] text-blue-900' />
+                                    </div>
+                                    <h2 className='flex flex-col text-left ml-2 font-bold text-[15px]'>
+                                        GDPR-
+                                        <br />
+                                        <span className='font-medium'>compliant</span>
+
+                                    </h2>
+                                </div>
+                                <div className='flex items-center ml-1 mt-2'>
+                                    <div className=' rounded-full '>
+                                        <ImDatabase className='text-[20px] ' />
+                                    </div>
+                                    <h2 className='flex flex-col text-left ml-2 font-bold text-[15px]'>
+                                        <span className='font-medium'>Hosted in</span>
+                                        Europe
+
+                                    </h2>
+                                </div>
                             </div>
-                            <div className='flex items-center ml-1 mt-1'>
-                                <div className='  rounded-full '>
-                                    <IoMdClock className='text-[2rem] text-blue-900' />
-                                </div>
-                                <h2 className='flex flex-col text-left ml-2 font-bold text-[15px]'>
-                                    GDPR-
-                                    <br />
-                                    <span className='font-medium'>compliant</span>
 
-                                </h2>
+                            <div className='text-center mt-[1.2rem]' >
+
+                                <button onClick={handleLogin} type="submit" disabled={!terms ? true : false} className={`${!terms && `disabled:opacity-50 disabled:cursor-not-allowed`} opacity-1 px-9 py-3 rounded-md text-white hover:bg-[#93C234] bg-[#E84025] `}>Login
+                                </button>
+
                             </div>
-                            <div className='flex items-center ml-1 mt-2'>
-                                <div className=' rounded-full '>
-                                    <ImDatabase className='text-[20px] ' />
-                                </div>
-                                <h2 className='flex flex-col text-left ml-2 font-bold text-[15px]'>
-                                    <span className='font-medium'>Hosted in</span>
-                                    Europe
 
-                                </h2>
+                            <div className='mt-[1.2rem]'>
+                                <div >
+                                    <span className='text-[14px] text-gray-400'>Phytomedicine </span>
+                                    <span className='text-[14px] ml-1 text-gray-400'>. Botanicals </span>
+                                    <span className='text-[14px] ml-1 text-gray-400'>. Cultivated Wellness </span>
+                                </div>
                             </div>
                         </div>
-
-                        <div className='text-center mt-[1.2rem]' >
-
-                            <button onClick={handleLogin} disabled={!terms ? true : false} className={`${!terms && `disabled:opacity-50 disabled:cursor-not-allowed`} opacity-1 px-9 py-3 rounded-md text-white hover:bg-[#93C234] bg-[#E84025] `}>Login
-                            </button>
-
-                        </div>
-
-                        <div className='mt-[1.2rem]'>
-                            <div >
-                                <span className='text-[14px] text-gray-400'>Phytomedicine </span>
-                                <span className='text-[14px] ml-1 text-gray-400'>. Botanicals </span>
-                                <span className='text-[14px] ml-1 text-gray-400'>. Cultivated Wellness </span>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
 
                 </div>
                 <div className='flex'>
