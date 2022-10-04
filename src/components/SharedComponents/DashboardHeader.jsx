8a54cf917954ee09d8f9d85529f3d3818dd1f7fd
laherasif <laherasif@gmail.com>
2022-10-04 04:24:55 -0700

    set header

--- src/components/SharedComponents/DashboardHeader.jsx
+++ src/components/SharedComponents/DashboardHeader.jsx
@@ -3,7 +3,7 @@ import { BiSearch } from "react-icons/bi";
 import { AiTwotoneCloud } from 'react-icons/ai'
 import { IoMdNotificationsOutline } from 'react-icons/io'
 import profileImage from '../../images/people.png'
-const DashboardHeader = ({ sidebarOpen, setSidebarOpen, checkExpand, setRightbarOpen, rightbarOpen }) => {
+const DashboardHeader = ({ sidebarOpen, setSidebarOpen, checkExpand, setRightbarOpen, rightbarOpen ,Toggle }) => {
     return (
         <header style={{ boxShadow: "0px 0px 6px #00000029" }} className="sticky top-0 lg:ml-1 bg-white border-b border-slate-200 z-30">
             <div className="px-4 sm:px-6 lg:px-6">
@@ -21,34 +21,55 @@ const DashboardHeader = ({ sidebarOpen, setSidebarOpen, checkExpand, setRightbar
 
                     {/* Header: Right side */}
                     <div className="lg:w-[100%] ">
-                        <div className='row top_search_bar '>
-                            <div className='col-lg-6 col-12 flex search_bar'>
-                                <input className='bg-[#F3F6FB] h-auto focus:outline-none lg:w-[100%] md:w-[100%] py-1 px-2 text-xs text-[#999FA9]' placeholder='search' />
-                                <button className='bg-[#E84025] rounded-sm lg:w-[8%] ml-2'><BiSearch className='inline text-white' /></button>
+                        <div className='flex justify-between items-center top_search_bar '>
+                            <div className='flex search_bar pt-1 w-[45%]'>
+                                <input onClick={() => Toggle(true)} className='bg-[#F3F6FB]  w-[500px] focus:outline-none  md:w-[100%] py-1 px-2 text-xs text-[#999FA9]' placeholder='search' />
+                                <button className='bg-green-700 h-[45px] rounded-sm lg:w-[8%] ml-2'><BiSearch className='inline text-white' /></button>
                             </div>
-                            <div className='col-lg-6 col-12  search_bar_items'>
-                                <div className={checkExpand ? "flex items-cente text-[10px]" : 'flex items-cente '}>
-                                    <div className='lg:flex lg:items-center lg:border-l  lg:p-2 border-r hidden'>
+                            <div className='  search_bar_items'>
+                                <div className={checkExpand ? "flex items-cente text-[10px]" : 'flex items-center '}>
+                                    <div className='lg:flex lg:items-center lg:p-2  hidden'>
                                         <AiTwotoneCloud className='text-[20px]' />
-                                        <span className='ml-2 text-sm'>Rain-1'C</span>
+                                        <span className='ml-2 text-[12px]'>Rain-1'C</span>
                                     </div>
-                                    <div className='lg:flex lg:items-center lg:border-l  lg:p-2 border-r hidden'>
-                                        <select className='focus:outline-none'>
-                                            <option>Islamabad</option>
-                                        </select>
-                                    </div>
-                                    <div className='lg:flex lg:items-center lg:border-l  lg:p-2 border-r hidden '>
-                                        <img src={profileImage} className=" ml-3 w-6" alt="profile" />
-                                        <select className='focus:outline-none'>
-                                            <option>Islamabad</option>
-                                        </select>
-                                    </div>
-                                    <div className='lg:flex lg:items-center  p-2 hidden'>
+                                    <div className='ml-2'>
                                         <IoMdNotificationsOutline className='text-[20px]' />
-                                        <img src={profileImage} className=" ml-3 w-6" alt="profile" />
-                                        <select className='focus:outline-none'>
+                                    </div>
+                                    <div className='ml-2'>
+                                        <IoMdNotificationsOutline className='text-[20px]' />
+                                    </div>
+                                    <div className='lg:flex lg:items-center border rounded-full ml-2  lg:p-1  hidden '>
+                                        <img src={profileImage} className="w-6" alt="profile" />
+                                        <select className=' text-[13px] focus:outline-none px-1'>
+                                            <option>Uk</option>
+                                        </select>
+                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" className="w-2 ml-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                            <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z" />
+                                        </svg>
+                                    </div>
+                                    <div className='lg:flex lg:items-center   lg:p-1 ml-2  hidden border rounded-full '>
+                                        <img src={profileImage} className=" w-6" alt="profile" />
+                                        <select className=' text-[13px] focus:outline-none px-1'>
+                                            <option>Dark Mode</option>
+                                        </select>
+                                    </div>
+                                    <div className='lg:flex lg:items-center   lg:p-1 border rounded-full ml-2  hidden '>
+                                        <img src={profileImage} className=" w-6" alt="profile" />
+                                        <select className=' text-[13px] focus:outline-none px-1'>
+                                            <option>English</option>
+                                        </select>
+                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" className="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                            <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z" />
+                                        </svg>
+                                    </div>
+                                    <div className='lg:flex lg:items-center p-1 ml-2 hidden'>
+                                        <img src={profileImage} className=" w-8" alt="profile" />
+                                        <select className=' text-[13px] focus:outline-none mx-1'>
                                             <option>Islamabad</option>
                                         </select>
+                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" className="w-2 ml-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                            <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z" />
+                                        </svg>
                                     </div>
 
 
