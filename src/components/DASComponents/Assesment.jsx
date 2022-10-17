cdc85639a3c3d1de9fed164842c4c3cb3816e1da
Muhammad Shakir <90838750+prodevXpert@users.noreply.github.com>
2022-10-18 00:36:31 +0500

    flow and design fixes

--- src/components/DASComponents/Assesment.jsx
+++ src/components/DASComponents/Assesment.jsx
@@ -1,7 +1,233 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRegUser, FaUnlockAlt, FaCheckCircle, FaCheckDouble } from "react-icons/fa";
 import { IoChevronForward } from "react-icons/io5";
 const Assesment = (props) => {
+
+    const [assessmentQuestions, setAssessmentQuestions] = useState([
+        {
+            id: 1,
+            question: "Question 1",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 2,
+            question: "Question 2",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 3,
+            question: "Question 3",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 4,
+            question: "Question 4",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 5,
+            question: "Question 5",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 6,
+            question: "Question 6",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 7,
+            question: "Question 7",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 8,
+            question: "Question 8",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 9,
+            question: "Question 9",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+        {
+            id: 10,
+            question: "Question 10",
+            options: [
+                {
+                    optionId: "A",
+                    option: "Alpha"
+                },
+                {
+                    optionId: "B",
+                    option: "Bravo"
+                },
+                {
+                    optionId: "C",
+                    option: "Charli"
+                },
+                {
+                    optionId: "D",
+                    option: "Doremon"
+                },
+            ]
+        },
+    ])
+    const [currentQuestion, setCurrentQuestion] = useState(assessmentQuestions[0]);
+    const [count, setCount] = useState(1);
+
     return (
         <div className='w-full px-4 lg:px-16'>
             <div style={{ boxShadow: "0px 3px 6px #00000029" }} className="border border-[#70707045]  pt-4 pb-1 lg:px-6 px-6 rounded-md font-sans">
@@ -49,8 +275,8 @@ const Assesment = (props) => {
                 <div className=' row g-0'>
 
                     <div style={{ boxShadow: "inset 0px 0px 0px #00000010, 0px 0px 6px #00000002" }} className='col-lg-12 mb-3 border border-[#70707045] p-3 '>
-                        <h1 className='text-[#626973] font-sans text-lg font-bold mb-2'>Question <span className='text-sm font-normal'>1 of 10</span></h1>
-                        <p className='text-sm pl-4 font-normal text-[#626973] mb-2'>It is a long established fact that a reader will be distracted by the readable content of a page ?</p>
+                        <h1 className='text-[#626973] font-sans text-lg font-bold mb-2'>Question <span className='text-sm font-normal'>{count} of {assessmentQuestions.length}</span></h1>
+                        <p className='text-sm pl-4 font-normal text-[#626973] mb-2'>{currentQuestion.question}</p>
                         <ul className='ml-10'>
                             <li>
                                 <label style={{ boxShadow: "0px 3px 6px #0000001A" }} className={`flex items-center mb-3 py-1  px-4  cursor-pointer text-sm   w-fit rounded-md `}>
@@ -58,7 +284,7 @@ const Assesment = (props) => {
                                         <span className={`rounded-[50%] w-[100%] h-[100%] inline-block bg-white`}></span>
                                     </span>
                                     <input type="radio" hidden name="provider" />
-                                    <span className="text-[#626973]">Option A</span>
+                                    <span className="text-[#626973]">{currentQuestion.options[0].option}</span>
                                 </label>
                             </li>
                             <li>
@@ -67,7 +293,7 @@ const Assesment = (props) => {
                                         <span className={`rounded-[50%] w-[100%] h-[100%] inline-block bg-white`}></span>
                                     </span>
                                     <input type="radio" hidden name="provider" />
-                                    <span className="text-[#626973]">Option A</span>
+                                    <span className="text-[#626973]">{currentQuestion.options[1].option}</span>
                                 </label>
                             </li>
                             <li>
@@ -76,7 +302,7 @@ const Assesment = (props) => {
                                         <span className={`rounded-[50%] w-[100%] h-[100%] inline-block bg-white`}></span>
                                     </span>
                                     <input type="radio" hidden name="provider" />
-                                    <span className="text-[#626973]">Option A</span>
+                                    <span className="text-[#626973]">{currentQuestion.options[2].option}</span>
                                 </label>
                             </li>
                             <li>
@@ -85,17 +311,20 @@ const Assesment = (props) => {
                                         <span className={`rounded-[50%] w-[100%] h-[100%] inline-block bg-white`}></span>
                                     </span>
                                     <input type="radio" hidden name="provider" />
-                                    <span className="text-[#626973]">Option A</span>
+                                    <span className="text-[#626973]">{currentQuestion.options[3].option}</span>
                                 </label>
                             </li>
                         </ul>
                     </div>
-
-
-
-
                     <div className='mb-3 text-right col-lg-12'>
-                        <button onClick={() => props.onNext("Dashboard")} className='font-sans font-medium text-lg border-2 border-[#4DA676] rounded px-16 py-1 text-[#4DA676] hover:text-white hover:bg-[#4DA676] transition-all'>Next <IoChevronForward className='inline' /></button>
+                        <button onClick={() => {
+                            if (count < assessmentQuestions.length) {
+                                setCount(count + 1)
+                                setCurrentQuestion(assessmentQuestions[count])
+                            } else if (count === assessmentQuestions.length) {
+                                props.onNext("Dashboard")
+                            }
+                        }} className='font-sans font-medium text-lg border-2 border-[#4DA676] rounded px-16 py-1 text-[#4DA676] hover:text-white hover:bg-[#4DA676] transition-all'>{count === assessmentQuestions.length ? "Submit" : "Next"} <IoChevronForward className='inline' /></button>
                     </div>
                 </div>
 
