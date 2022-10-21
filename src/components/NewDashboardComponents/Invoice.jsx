a7c94f47317d3f643749e56778a8f19003053b3b
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-10-22 00:35:19 +0500

    Update Invoice.jsx

--- src/components/NewDashboardComponents/Invoice.jsx
+++ src/components/NewDashboardComponents/Invoice.jsx
@@ -8,20 +8,20 @@ const Invoice = () => {
                     <div className='col-lg-2 text-[#707070] font-semibold'>Earnings</div>
                     <div className='col-lg-10'>
                         <div className='p-4 mt-8 text-center border rounded-xl row g-0'>
-                            <div className='border col-lg-3'>
+                            <div className='col-lg-3'>
                                 <h1 className='text-[#464A53] font-bold text-lg font-sans'>Total Invoice</h1>
                                 <h1 className='text-[#4DA676] font-bold font-sans'>$13250</h1>
                             </div>
-                            <div className='border col-lg-3'>
-                                <h1 className='text-[#464A53] font-bold text-lg font-sans'>Total Invoice</h1>
-                                <h1 className='text-[#4DA676] font-bold font-sans'>$13250</h1>
+                            <div className='col-lg-3'>
+                                <h1 className='text-[#464A53] font-bold text-lg font-sans'>Overdue</h1>
+                                <h1 className='text-[#E84125] font-bold font-sans'>$13250</h1>
                             </div>
-                            <div className='border col-lg-3'>
-                                <h1 className='text-[#464A53] font-bold text-lg font-sans'>Total Invoice</h1>
-                                <h1 className='text-[#4DA676] font-bold font-sans'>$13250</h1>
+                            <div className='col-lg-3'>
+                                <h1 className='text-[#464A53] font-bold text-lg font-sans'>Paid</h1>
+                                <h1 className='text-[#5D89ED] font-bold font-sans'>$13250</h1>
                             </div>
-                            <div className='border col-lg-3'>
-                                <h1 className='text-[#464A53] font-bold text-lg font-sans'>Total Invoice</h1>
+                            <div className='col-lg-3'>
+                                <h1 className='text-[#464A53] font-bold text-lg font-sans'>Unpaid</h1>
                                 <h1 className='text-[#4DA676] font-bold font-sans'>$13250</h1>
                             </div>
                         </div>
