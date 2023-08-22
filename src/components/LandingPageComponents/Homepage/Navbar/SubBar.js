745c2888835025c5f34762ab7f68f068c7ac43d2
M Usman Haider <114842526+haiderusman147@users.noreply.github.com>
2023-08-22 15:29:23 +0500

    Update SubBar.js

--- src/components/LandingPageComponents/Homepage/Navbar/SubBar.js
+++ src/components/LandingPageComponents/Homepage/Navbar/SubBar.js
@@ -110,15 +110,7 @@ const SubBar = ({ setShow }) => {
         completed during 2022. */}
       </Marquee>
     </div>
-    <DMarquee className='bg-[#C9E065] '>
-                <div className='py-2 flex items-center'>
-                <div className='flex items-center mx-5'>
-                This website is currently under development. Please return soon!    Esta página web se encuentra en desarrollo. ¡Por favor regrese pronto!    Deze website is momenteel in ontwikkeling. Kom snel terug!    Este site está em desenvolvimento. ¡Por favor, volte logo!    Diese Website befindet sich derzeit im AuYau. Bitte kehren Sie bald zurück!    Questo sito web è attualmente in fase di sviluppo. Si prega di tornare presto!     Tämä sivusto on parhaillaan kehitteillä. Palaa pian!    该网站目前正在开发中。请尽快回来!     y اذه عقوملا دiق رmnطتلا اiلاح . ءاجرلا ةدوعلا اwnxق
-                    
-                    </div>
-
-                </div>
-            </DMarquee>
+   
      
       <TopBar setShow={setShow} />
       {user?.gatePopup === false && popup ? (<AgeGatePopup isOpen={popup} onClose={() => { setPopup(false) }} />) : null}
